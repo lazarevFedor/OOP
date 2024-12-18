c33b4ad8c34f02b4b6df74d9fd167bbb75355dc9
Фёдор <147192648+lazarevFedor@users.noreply.github.com>
2024-12-19 00:38:49 +0300

    Delete src directory

--- src/lab7/3372_Лазарев_№7/OOP_QT_Project-master/client/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <QTranslator>
-#include "application.h"
-
-
-int main(int argc, char *argv[]) {
-    ClientApplication a(argc, argv);
-    return a.exec();
-}
--- src/lab7/3372_Лазарев_№7/OOP_QT_Project-master/common/common.cpp
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-
-
-
-QString& operator<< (QString& m, const QString& s) {
-    m += s;
-    m.append(separator);
-    return m;
-}
